6cfa643ea926ce04f60504d83c513f5b20fcd739
Claudio Curieses <claudy_curi_10@hotmail.com>
2017-01-06 20:59:18 +0100

    Correccio columna "nom" per "tipus" (assistencia)

--- PassarLlista/app/src/main/java/edu/upc/epsevg/passarllista/base_de_dades/Contracte_Assistencia.java
+++ PassarLlista/app/src/main/java/edu/upc/epsevg/passarllista/base_de_dades/Contracte_Assistencia.java
@@ -7,7 +7,7 @@ public class Contracte_Assistencia {
     // Creem la classe interna Entrada Assistencia per a guardar els noms de les columnes de la taula
     public static abstract class EntradaAssistencia implements BaseColumns {
         public static final String TABLE_NAME ="assistencia";
-        public static final String TIPUS = "nom";
+        public static final String TIPUS = "tipus";
         public static final String ID_ALUMNE = "id_alumne";
         public static final String ID_SESSIO = "id_sessio";
     }
